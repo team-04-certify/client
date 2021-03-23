1757f2c5c791f93e60a49aab35f888b0922cac69
ardyanrizki <surel.ardy@gmail.com>
2021-03-23 13:27:00 +0700

    finishing recipient crud

--- certify/src/Store/Actions/OrganizersAction.js
+++ certify/src/Store/Actions/OrganizersAction.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3000";
+const baseUrl = "http://localhost:4000";
 
 const setLoading = (payload) => {
   return { type: "SET_LOADING", payload };
