0ea5baef6a28d20ae9df8bfa180bcff7b334b1e4
Agus Riyanto <agusriyanto@Users-MacBook-Pro.local>
2021-03-25 19:11:41 +0700

    fix bug

--- certify/src/Components/index.js
+++ certify/src/Components/index.js
@@ -1,6 +1,7 @@
 export { default as NavbarHome } from "./NavbarHome";
 export { default as NavbarDashboard } from "./NavbarDashboard";
 export { default as NavbarInformation } from "./NavbarInformation";
+export { default as NavbarValidate } from "./NavbarValidate";
 export { default as EventCard } from "./EventCard";
 export { default as InformationCard } from "./InformationCard";
 export { default as RecipientRow } from "./RecipientRow";
